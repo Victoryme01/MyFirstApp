739a8c701c0f9fd787929e23419a0d65d6ee78f2
Saroj sharma <jangosaroj@gmail.com>
2022-02-16 15:07:36 +0545

    sale

---
+++ app/src/main/java/sarojsharma/choice/myfirstapp/MainActivity.java
@@ -0,0 +1,17 @@
+package sarojsharma.choice.myfirstapp;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+WebView web;
+TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        web = findViewById(R.id.myweb);
+        web.loadUrl("https://sharesansar.com/");
+
+    }
+}
